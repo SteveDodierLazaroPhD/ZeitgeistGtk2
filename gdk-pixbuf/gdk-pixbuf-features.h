ad68b50ae764fffb4fa1b34bbcf6eac9f78d8eb9
Bazaar Package Importer <james.westby@ubuntu.com>
2008-09-24 17:26:40 +0200

    mestamp: Wed 2008-09-24 17:26:40 +0200 ssage: * New upstream version:   - Revert problematic GtkAdjustment changes   - Bugs fixed:   552837 mem leak in gtkimmulticontext   553000 incorrect i18n header in gtkfilesystem.c   553135 eog crash: assertion failed (lp: #272754)   552545 leaks GpImage * debian/patches/092_gtkadjustement_doesnt_use_page_size_value.patch:   - the new version has a similar change ---------------------------------------------------------- e --include-merged or -n0 to see merged revisions.
    original bzr status:
    revno: 91 [merge]
    fixes bug: https://launchpad.net/bugs/272754
    author: Sebastien Bacher <seb128@ubuntu.com>
    committer: Bazaar Package Importer <james.westby@ubuntu.com>

--- gdk-pixbuf/gdk-pixbuf-features.h
+++ gdk-pixbuf/gdk-pixbuf-features.h
@@ -7,8 +7,8 @@
 
 #define GDK_PIXBUF_MAJOR (2)
 #define GDK_PIXBUF_MINOR (14)
-#define GDK_PIXBUF_MICRO (2)
-#define GDK_PIXBUF_VERSION "2.14.2"
+#define GDK_PIXBUF_MICRO (3)
+#define GDK_PIXBUF_VERSION "2.14.3"
 
 /* We prefix variable declarations so they can
  * properly get exported/imported from Windows DLLs.
