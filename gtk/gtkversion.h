e48dee3c054e685e5c58e8087e8452a6f76639f9
Bazaar Package Importer <james.westby@ubuntu.com>
2011-04-04 20:37:35 +1000

    ssage: * New upstream release * debian/patches/096_git_combo_hasentry.patch:   - Applied upstream ---------------------------------------------------------- e --include-merged or -n0 to see merged revisions.
    original bzr status:
    revno: 217 [merge]
    author: Robert Ancell <robert.ancell@canonical.com>
    committer: Bazaar Package Importer <james.westby@ubuntu.com>
    branch nick: natty

--- gtk/gtkversion.h
+++ gtk/gtkversion.h
@@ -35,9 +35,9 @@
  */
 #define GTK_MAJOR_VERSION				(2)
 #define GTK_MINOR_VERSION				(24)
-#define GTK_MICRO_VERSION				(3)
-#define GTK_BINARY_AGE					(2403)
-#define GTK_INTERFACE_AGE				(3)
+#define GTK_MICRO_VERSION				(4)
+#define GTK_BINARY_AGE					(2404)
+#define GTK_INTERFACE_AGE				(4)
 
 /* check whether a Gtk+ version equal to or greater than
  * major.minor.micro is present.
